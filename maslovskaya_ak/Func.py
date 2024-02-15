5bca64d9d3a9ac2f2db1cff1b2383adba69d8d14
Cgsgam3_Ana <149404177+Alsh111@users.noreply.github.com>
2024-02-15 14:59:21 +0300

    Update Func.py

--- maslovskaya_ak/Func.py
+++ maslovskaya_ak/Func.py
@@ -2,20 +2,15 @@ import math
     
 def Func(S):
     L = 0
-    R = 1
+    R = 0
     N = int(S)
-    Fx0 = 200
     Fx1 = 0
 
-    while abs(Fx1 - Fx0) > 0.4:
-        R = L
-        Fx0 = Fx1
-        Fx1 = 0
-        for n in range (N):
-            len = 1 / N
-            R +=  L + len
-            Fx1 += math.log((L + R)/2 + 1) * len
+    for n in range (N):
+        len = 1 / N
+        R += len
+        Fx1 += math.log((L + R)/2 + 1) * len
     return Fx1
-
+    
 #N = float(input("[0;1]"))
 #print(Func(N))
