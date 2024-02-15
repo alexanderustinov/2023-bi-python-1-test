a70117d02d622e6ba24be601d4b530f9434a7bf5
Cgsgam3_Ana <149404177+Alsh111@users.noreply.github.com>
2024-02-15 14:50:55 +0300

    Update Func.py

--- maslovskaya_ak/Func.py
+++ maslovskaya_ak/Func.py
@@ -1,17 +1,21 @@
 import math
     
-def Func(x):
-    fx = math.pi/2
+def Func(S):
+    L = 0
+    R = 1
+    N = int(S)
+    Fx0 = 200
     Fx1 = 0
-    i = 1
-    fx -= ((-1)**0) * x**(1) / (1)
-    
-    while abs(Fx1 - fx) > 0.001:
-        Fx1 = fx
-        fx -= ((-1)**(i + 0)) * x**(2*i + 1) / (2*i + 1)
-        i += 1
-    return fx
+
+    while abs(Fx1 - Fx0) > 0.4:
+        R = L
+        Fx0 = Fx1
+        Fx1 = 0
+        for n in range (N):
+            len = 1 / N
+            R +=  L + len
+            Fx1 += math.log((L + R)/2 + 1) * len
+    return Fx1
 
 #N = float(input("[0;1]"))
 #print(Func(N))
-#print(math.pi / 2 - math.atan(N))
