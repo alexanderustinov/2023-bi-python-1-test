6916183ea89b8922968bcacd6e514635d467e7c0
cwerti <117809737+cwerti@users.noreply.github.com>
2024-02-15 14:56:27 +0300

    Add files via upload

--- dyadechkov_iv/main.py
+++ dyadechkov_iv/main.py
@@ -1,14 +1,5 @@
-import math
+"""Module providing a function printing python version."""
+from integral import integral
 
-e = math.e
-
-
-def teylor(x):
-    s = e + e * (x + 1)
-    for i in range(2, 100):
-        s += ((x - 1) ** 2) * e / math.factorial(i)
-    return round(s, 3)
-
-
-def test_teylor():
-    assert teylor(1) == 8.155
+step = int(input())
+print(integral(step))
---
+++ dyadechkov_iv/tests.py
@@ -0,0 +1,6 @@
+from integral import integral
+import pytest
+
+@pytest.mark
+def test_integral():
+    assert integral(2) == 1.324360635350064
